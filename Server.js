aeef680cb04a48c6166b98f32fc3a85ab32f80d4
adaomagalhaes03 <adaomagalhaes793@gmail.com>
2025-01-28 14:48:25 +0100

    feat: server is running...

--- Server.js
+++ Server.js
@@ -1,8 +1,13 @@
 'Use strict'
+const http = require('http');
+const debug = require('debug')('nodestr:server');
 const express = require('express');
 
 const app = express();
+// aplicando a normalização da porta para rodar no servidor local
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
-app.get('/', (req, res) => {    
-    res.send('Bem vindo');
-});
+const server = http.createServer(app);
+const router = express.Router();
+
