c2795502d4c78d0f359b9b09ed8d850c71d2ab13
adaomagalhaes03 <adaomagalhaes793@gmail.com>
2025-01-28 14:54:04 +0100

    feat: dubug in server

--- Server.js
+++ Server.js
@@ -11,3 +11,17 @@ app.set('port', port);
 const server = http.createServer(app);
 const router = express.Router();
 
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API",
+        version: "0.0.1"
+    });
+}); 
+ 
+app.use('/', route);
+
+server.listen(port);
+server.on('error', onError);
+// função para debugar a porta
+console.log('API rodando na porta ' + port);
+
