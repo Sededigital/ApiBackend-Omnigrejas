f257d63c6fb8f2e93fca69c411bb34957c7da4b8
adaomagalhaes03 <adaomagalhaes793@gmail.com>
2025-01-30 18:50:41 +0100

    feat: routers

--- source/routes/usuarios.js
+++ source/routes/usuarios.js
@@ -3,12 +3,12 @@
 const express = require("express");
 const router = express.Router(); 
 
- const create = router.post('/', (req, res) => {
+ const create = router.post('/', (req, res, next) => {
     res.status(201).send(req.body); 
 }); 
 
 
-const put = router.put('/:id', (req, res) => {
+const put = router.put('/:id', (req, res, next) => {
     const id = req.params.id;
     res.status(200).send({
         id: id,
@@ -18,7 +18,7 @@ const put = router.put('/:id', (req, res) => {
 });
 
 // Rota para exclusão de dados
-const del = router.delete('/:id', (req, res) => { 
+const del = router.delete('/:id', (req, res, next) => { 
     const id = req.params.id;
     res.status(200).send({
         message: `Item com ID ${id} foi excluído com sucesso!`
