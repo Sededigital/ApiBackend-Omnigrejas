5acbf4cc95b2df5a79b9c46034066a8fdeb6a0e9
adaomagalhaes03 <adaomagalhaes793@gmail.com>
2025-02-10 21:13:35 +0100

    feat: connect mongoose

--- source/Config/database.js
+++ source/Config/database.js
@@ -7,3 +7,24 @@ const DBname = process.env.DB_NAME;
 const DBhost = process.env.DB_HOST;
 const DBport = process.env.DB_PORT;
 
+const connect = async () => {
+    try {
+        await mongoose.connect(`mongodb://${DBuser}:${DBpassword}@${DBhost}:${DBport}/${DBname}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        });
+        console.log("🔥 MongoDB conectado com sucesso!");
+    } catch (err) {
+        console.error("❌ Erro ao conectar ao MongoDB:", err);
+    }
+    };
+
+    const  disconnect = async () => {
+        await mongoose.disconnect();
+    };
+
+    const connection = mongoose.connection;
+
+    module.exports = { connect, disconnect };
+
+
