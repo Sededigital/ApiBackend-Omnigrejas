890f20a99d368a1c03492506a2fccb53dae591e0
adaomagalhaes03 <adaomagalhaes793@gmail.com>
2025-02-13 14:31:39 +0100

    feat: module user

--- source/routes/usuarios-routers.js
+++ source/routes/usuarios-routers.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+  const Uer = require('../models/User');
 const bcrypt = require('bcrypt'); // Para hashear a senha
 
 // Criar um novo usuário
